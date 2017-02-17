018c53dfa0e42adab1254db1f8315d2572d059bd
sokom heng <sokom heng>
2017-02-17 21:41:47 +0100

    fb composer and homepage background is added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,103 +7,104 @@
     {!! Html::style('frontend/css/pages/page_pricing.css?'.time()) !!}
 @stop
 @section('body')
-    <div class="container home-container content">
-        <h1 class="text-center margin-bottom-35">FIND OR POST A JOB</h1>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="profile-body margin-bottom-20">
-                    <div class="tab-v1">
-                        <ul class="nav nav-justified nav-tabs">
-                            <li class="active"><a data-toggle="tab" href="#search_job" aria-expanded="false">SEARCH JOBS</a></li>
-                            <li><a data-toggle="tab" href="#post_job" aria-expanded="true">POST JOBS</a></li>
-                        </ul>
-                        <div class="tab-content">
-                            <div id="search_job" class="panel-body tab-pane fade active in">
-                                {!! Form::open(['url' => route('jobs.index'), 'id' => 'search_job', 'method' => 'GET']) !!}
-                                <div class="form-group">
-                                    <div class="col-md-4">
-                                        {{ Form::select('category', $categories, null, ['class' => 'form-control input-lg rounded']) }}
+    <section class="home-section">
+        <div class="container home-container content-sm">
+            <h1 class="text-center margin-bottom-35 color-white">FIND OR POST A JOB</h1>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="profile-body margin-bottom-20">
+                        <div class="tab-v1">
+                            <ul class="nav nav-justified nav-tabs">
+                                <li class="active"><a data-toggle="tab" href="#search_job" aria-expanded="false">SEARCH JOBS</a></li>
+                                <li><a data-toggle="tab" href="#post_job" aria-expanded="true">POST JOBS</a></li>
+                            </ul>
+                            <div class="tab-content">
+                                <div id="search_job" class="panel-body tab-pane fade active in">
+                                    {!! Form::open(['url' => route('jobs.index'), 'id' => 'search_job', 'method' => 'GET']) !!}
+                                    <div class="form-group">
+                                        <div class="col-md-4">
+                                            {{ Form::select('category', $categories, null, ['class' => 'form-control input-lg rounded']) }}
+                                        </div>
+                                        <div class="col-md-4">
+                                            {{ Form::select('location', $locations, null, ['class' => 'form-control input-lg rounded']) }}
+                                        </div>
+                                        <div class="col-md-2">
+                                            <button type="submit" class="btn btn-primary rounded btn-lg">Search</button>
+                                        </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        {{ Form::select('location', $locations, null, ['class' => 'form-control input-lg rounded']) }}
-                                    </div>
-                                    <div class="col-md-2">
-                                        <button type="submit" class="btn btn-primary rounded btn-lg">Search</button>
-                                    </div>
+                                    {!! Form::close() !!}
                                 </div>
-                                {!! Form::close() !!}
-                            </div>
-                            <div id="post_job" class="panel-body tab-pane fade in">
-                                @if (Auth::check())
-                                {!! Form::open(['url' => route('jobs.store'), 'class' => 'create-job', 'data-parsley-validate', 'id' => 'create_job']) !!}
-                                <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                                    {{ Form::label('title', 'Job Title') }}
-                                    {{ Form::text('title', null, array('class' => 'input-lg form-control rounded', 'id' => 'title', 'required' => 'true')) }}
-                                    @if ($errors->has('title'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('title') }}</strong>
+                                <div id="post_job" class="panel-body tab-pane fade in">
+                                    @if (Auth::check())
+                                    {!! Form::open(['url' => route('jobs.store'), 'class' => 'create-job', 'data-parsley-validate', 'id' => 'create_job']) !!}
+                                    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                                        {{ Form::label('title', 'Job Title') }}
+                                        {{ Form::text('title', null, array('class' => 'input-lg form-control rounded', 'id' => 'title', 'required' => 'true')) }}
+                                        @if ($errors->has('title'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('title') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                {{ Form::label('category', 'Category') }}
+                                                {{ Form::select('category_id', $categories, null, ['class' => 'form-control input-lg rounded']) }}
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label>Area / Suburb</label>
+                                                {{ Form::select('area_suburb_id', $locations, null, ['class' => 'form-control input-lg rounded']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                                        {{ Form::label('description', 'Short Description') }}
+                                        <textarea type="text" class="form-control input-lg rounded" name="description" ></textarea>
+                                        @if ($errors->has('description'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('description') }}</strong>
                                         </span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            {{ Form::label('category', 'Category') }}
-                                            {{ Form::select('category_id', $categories, null, ['class' => 'form-control input-lg rounded']) }}
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>Area / Suburb</label>
-                                            {{ Form::select('area_suburb_id', $locations, null, ['class' => 'form-control input-lg rounded']) }}
-                                        </div>
+                                        @endif
                                     </div>
-                                </div>
-                                <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-                                    {{ Form::label('description', 'Short Description') }}
-                                    <textarea type="text" class="form-control input-lg rounded" name="description" ></textarea>
-                                    @if ($errors->has('description'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('description') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="form-group{{ $errors->has('photo_ids') ? ' has-error' : '' }}">
-                                    <label>Add Photos</label>
-                                    <div class="dropzone-previews dropzone" id="dropzone_preview" data-url="{{ URL::route('jobs.upload_photo') }}"></div>
-                                    @if ($errors->has('description'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('photo_ids') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="form-group text-right">
-                                    <button type="submit" class="btn btn-primary rounded btn-lg">POST</button>
-                                </div>
+                                    <div class="form-group{{ $errors->has('photo_ids') ? ' has-error' : '' }}">
+                                        <label>Add Photos</label>
+                                        <div class="dropzone-previews dropzone" id="dropzone_preview" data-url="{{ URL::route('jobs.upload_photo') }}"></div>
+                                        @if ($errors->has('description'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('photo_ids') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group text-right">
+                                        <button type="submit" class="btn btn-primary rounded btn-lg">POST</button>
+                                    </div>
 
-                            {!! Form::close() !!}
-                                @else
-                                    <h2 class="text-center color-white margin-bottom-15">Oops, you are not logged in!</h2>
-                                    <h3 class="text-center color-white margin-bottom-20"><b>Sign In below!</b></h3>
-                                    <div class="row">
-                                        <div class="col-md-8 col-md-offset-2">
-                                            @include('partial/login_form')
+                                {!! Form::close() !!}
+                                    @else
+                                        <h2 class="text-center color-white margin-bottom-15">Oops, you are not logged in!</h2>
+                                        <h3 class="text-center color-white margin-bottom-20"><b>Sign In below!</b></h3>
+                                        <div class="row">
+                                            <div class="col-md-8 col-md-offset-2">
+                                                @include('partial/login_form')
+                                            </div>
                                         </div>
-                                    </div>
-                                    <h3 class="text-center color-white">Dont have an account? Its <b>FREE!</b></h3>
-                                    <h3 class="text-center color-white margin-bottom-20"><b>Sign Up below!</b></h3>
-                                    <div class="row">
-                                        <div class="col-md-10 col-md-offset-1">
-                                            @include('partial/register_form')
+                                        <h3 class="text-center color-white">Dont have an account? Its <b>FREE!</b></h3>
+                                        <h3 class="text-center color-white margin-bottom-20"><b>Sign Up below!</b></h3>
+                                        <div class="row">
+                                            <div class="col-md-10 col-md-offset-1">
+                                                @include('partial/register_form')
+                                            </div>
                                         </div>
-                                    </div>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
+    </section>
     <!--=== Service Block ===-->
     <div class="container content-sm how-works-container">
         <h2 class="text-center">How TRADEE works</h2>

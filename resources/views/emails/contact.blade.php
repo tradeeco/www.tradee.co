b5c1b12960e794ac5c3e96f678e27cfe5858f8ab
sokom heng <sokom heng>
2017-02-27 18:34:19 +0100

    email template test

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,3 +1,30 @@
-<div>
-    Price: I'm good
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    {!! Html::style('frontend/plugins/bootstrap/css/bootstrap.min.css') !!}
+</head>
+<body>
+<h2>This is weekly analytics about the jobs</h2>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Job Detail</th>
+                <th>Job content</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>I'm good</td>
+                <td>I'm good</td>
+                <td>I'm good</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
+<hr/>
+<b>From Support Team</b>
+</body>
+</html>

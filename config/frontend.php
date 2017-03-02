294d3804dac67e9ee887fa5c3c2c12d8b1a25515
sokom heng <sokom heng>
2017-03-02 02:40:00 +0100

    job per page number is changed

--- config/frontend.php
+++ config/frontend.php
@@ -5,7 +5,7 @@ return [
     'icon_size'   => env('UPLOAD_ICON_SIZE'),
     'job_photo_path' => env('JOB_PHOTO_PATH'),
     'user_avatar_path' => env('USER_AVATAR_PATH'),
-    'job_per_page' => '1',
+    'job_per_page' => '15',
     'lengths' => [
         '' => '',
         '1' => '1 year',

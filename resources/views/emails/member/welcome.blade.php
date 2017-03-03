68e45f06a14cb2f7482b2f55a7360aea8a13c5c6
sokomheng <sokomheng89@gmail.com>
2017-03-03 21:28:59 +0100

    email content is fixed

--- resources/views/emails/member/welcome.blade.php
+++ resources/views/emails/member/welcome.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    @include('emails/bootstrap_css');
+    @include('emails/bootstrap_css')
 </head>
 <body>
-<h1>Welcome to <a href="{{ config('app.url') }}">{{ config('app.name') }}</a></h1>
+<h3>Welcome to <a href="{{ config('app.url') }}">{{ config('app.name') }}</a></h3>
 <p>
     Thank you for signing up with TRADEE! Your new account is now fully setup and you are ready to start posting or searching for jobs!
 </p>
@@ -13,7 +13,6 @@
     Help us bring more communities together at <a href="{{ config('app.url') }}">www.tradee.co!</a>
 </P>
 <P>Kind regards</P>
-<hr/>
 <b>TRADEE Team</b>
 </body>
 </html>
